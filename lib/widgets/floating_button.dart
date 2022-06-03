0c639b8606ac2d620dd4a49a7de1cadb52aaf970
0hyunU <ryh2803@gmail.com>
2022-06-03 22:14:56 +0900

    separate floating button widget

---
+++ lib/widgets/floating_button.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+
+class MultiFloatButton extends StatelessWidget {
+  final double _icon_size = 28;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SpeedDial(animatedIcon: AnimatedIcons.menu_close,
+
+        // activeIcon: Image.asset("assets/icons/heartbeat.png"),
+        // child:Image.asset('assets/icons/heartbeat.png',
+        // height: _icon_size + 3 , color: Colors.white),
+        children: [
+          SpeedDialChild(
+            child: Image.asset('assets/icons/blooddrop.png',
+                height: _icon_size * 0.9, color: Colors.black),
+            //Icon(Icons.chrome_reader_mode, color: Colors.black),
+            backgroundColor: Colors.white,
+            onTap: () => print('blood pressure'),
+            label: 'Blood Pressure',
+            labelStyle:
+                TextStyle(fontWeight: FontWeight.w500, color: Colors.black),
+            labelBackgroundColor: Colors.white,
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.monitor_heart_outlined,color: Colors.black), 
+            //Image.asset('assets/icons/heartbeat.png',
+                // height: _icon_size * 0.9, color: Colors.black),
+            //Icon(Icons.create, color: Colors.black),
+            backgroundColor: Colors.white,
+            onTap: () => print('ECG'),
+            label: 'ECG',
+            labelStyle:
+                TextStyle(fontWeight: FontWeight.w500, color: Colors.black),
+            labelBackgroundColor: Colors.white,
+          ),
+        ]);
+  }
+}
